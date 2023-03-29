a5cd8f8a839d69782c550f009707eea52b6a4924
yogjun <280536928@qq.com>
2023-03-29 15:36:05 +0800

    commit slf4j

--- bean-handler/src/test/java/jsonpath/JsonPathTest.java
+++ bean-handler/src/test/java/jsonpath/JsonPathTest.java
@@ -17,6 +17,13 @@ public class JsonPathTest {
       "{\"store\":{\"book\":[{\"category\":\"reference\",\"author\":\"NigelRees\",\"title\":\"SayingsoftheCentury\",\"price\":8.95},{\"category\":\"fiction\",\"author\":\"EvelynWaugh\",\"title\":\"SwordofHonour\",\"price\":12.99},{\"category\":\"fiction\",\"author\":\"HermanMelville\",\"title\":\"MobyDick\",\"isbn\":\"0-553-21311-3\",\"price\":8.99},{\"category\":\"fiction\",\"author\":\"J.R.R.Tolkien\",\"title\":\"TheLordoftheRings\",\"isbn\":\"0-395-19395-8\",\"price\":22.99}],\"bicycle\":{\"color\":\"red\",\"price\":19.95}},\"expensive\":10}";
 
   @Test
+  public void refs() {
+    String newJson =
+        JsonPath.parse(json).set("$['store']['book'][0]['author']", "Paul").jsonString();
+    System.out.println(newJson);
+  }
+
+  @Test
   public void testFromBean() {
     List<String> authors = JsonPath.read(json, "$.store.book[*].author");
     System.out.println(authors);
