a9a10e68b2d1806f3466ff4084c449edc9d7066b
yogjun <280536928@qq.com>
2023-03-31 16:03:35 +0800

    add modules

---
+++ function-manager/src/main/java/cn/yogjun/docking/function/manager/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * {@link package-info}
+ *
+ * @author <a href="mailto:matthew.miao@yunlsp.com">matthew.miao</a>
+ * @version ${project.version} - 2023/3/31
+ */
+package cn.yogjun.docking.function.manager;
