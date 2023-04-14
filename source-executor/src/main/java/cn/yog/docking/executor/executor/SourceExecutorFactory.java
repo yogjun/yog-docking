a0e2e0b3775e0a36136181746653c04e20a7d5a4
yogjun <280536928@qq.com>
2023-04-14 13:57:27 +0800

    modify source name

--- source-executor/src/main/java/cn/yog/docking/executor/executor/SourceExecutorFactory.java
+++ source-executor/src/main/java/cn/yog/docking/executor/executor/SourceExecutorFactory.java
@@ -1,8 +1,5 @@
 package cn.yog.docking.executor.executor;
 
-import cn.hutool.extra.spring.SpringUtil;
-import cn.yogjun.docking.bean.enums.SourceType;
-
 /**
  * {@link SourceExecutorFactory}
  *
@@ -13,11 +10,11 @@ public class SourceExecutorFactory {
 
   private SourceExecutorFactory() {}
 
-  public static <T> SourceExecutor<T> getExecutor(SourceType sourceType) {
-    return SpringUtil.getBean(sourceType.getHandlerName());
-  }
-
-  public static <T> SourceExecutor<T> getExecutor(Class<T> clazz) {
-    return SpringUtil.getBean(clazz.getSimpleName());
-  }
+  //  public static <T> SourceExecutor<T> getExecutor(SourceType sourceType) {
+  //    return SpringUtil.getBean(sourceType.getHandlerName());
+  //  }
+  //
+  //  public static <T> SourceExecutor<T> getExecutor(Class<T> clazz) {
+  //    return SpringUtil.getBean(clazz.getSimpleName());
+  //  }
 }
