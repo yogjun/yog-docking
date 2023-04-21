41574a756ce86114db645c5416881c7419c1a3e6
yogjun <280536928@qq.com>
2023-04-21 15:47:55 +0800

    add execute listener interface

--- source-executor/src/main/java/cn/yog/docking/executor/listener/ExecutorListener.java
+++ source-executor/src/main/java/cn/yog/docking/executor/listener/ExecutorListener.java
@@ -4,14 +4,18 @@ import cn.yog.docking.executor.param.Params;
 import cn.yogjun.docking.source.manager.bean.base.SourceBean;
 
 /**
- * {@link SourceListener} 执行整个资源任务的监听器
+ * {@link ExecutorListener} 执行单个source spec的监听器
  *
  * @author <a href="mailto:matthew.miao@yunlsp.com">matthew.miao</a>
  * @version ${project.version} - 2023/4/13
  */
-public interface SourceListener {
+public interface ExecutorListener {
 
   void beforeExecute(SourceBean source, Params param);
 
   void afterExecute(SourceBean source, Params param);
+
+  void onSuccess(SourceBean source, Params param);
+
+  void onFailure(SourceBean source, Params param, Exception e);
 }
